60acd9380279392a95c53821faa4584cc8c9f87e
Carlos Melendez <carlosmelendezr@gmail.com>
2023-05-07 17:25:28 -0400

    Detalle de la lista

---
+++ app/src/main/java/com/veramed/inventario/ui/lista/ListaDetalleViewModel.kt
@@ -0,0 +1,92 @@
+package com.veramed.inventario.ui.lista
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.veramed.inventario.data.Item
+import com.veramed.inventario.data.ItemsRepository
+import com.veramed.inventario.data.ListaItemRepository
+import com.veramed.inventario.data.ListaItems
+import com.veramed.inventario.ui.item.*
+import com.veramed.inventario.ui.item.toItemDetails
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel to retrieve, update and delete an item from the [ItemsRepository]'s data source.
+ */
+class ListaDetalleViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val listaItemRepository: ListaItemRepository,
+) : ViewModel() {
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemDetailsDestination.itemIdArg])
+
+    /**
+     * Holds the item details ui state. The data is retrieved from [ItemsRepository] and mapped to
+     * the UI state.
+     */
+    val detalleUiState: StateFlow<ListaItemDetalleUiState> =
+        listaItemRepository.getItemLista(itemId)
+            .filterNotNull()
+            .map {
+                ListaItemDetalleUiState(outOfStock =false, itemDetalle = it.first().toItemDetails())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ListaItemDetalleUiState()
+            )
+
+    /**
+     * Reduces the item quantity by one and update the [ItemsRepository]'s data source.
+     */
+   /* fun reduceQuantityByOne() {
+        viewModelScope.launch {
+            val currentItem = detalleUiState.value.itemDetails.toItem()
+            if (currentItem.quantity > 0) {
+                listaItemRepository.updateItem(currentItem.copy(quantity = currentItem.quantity - 1))
+            }
+        }
+    }*/
+
+    /**
+     * Deletes the item from the [ItemsRepository]'s data source.
+     */
+    suspend fun deleteItem() {
+        listaItemRepository.deleteItem(detalleUiState.value.itemDetalle.toItem())
+    }
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+/**
+ * UI state for ItemDetailsScreen
+ */
+data class ListaItemDetalleUiState(
+    val outOfStock: Boolean = true,
+    val itemDetalle: ListaItemDetails = ListaItemDetails()
+)
+
+/**
+ * Extension function to convert [Item] to [ItemUiState]
+ */
+fun Item.toItemUiState(isEntryValid: Boolean = false): ItemUiState = ItemUiState(
+    itemDetails = this.toItemDetails(),
+    isEntryValid = isEntryValid
+)
+
+/**
+ * Extension function to convert [Item] to [ItemDetails]
+ */
+fun ListaItems.toItemDetails(): ListaItemDetails = ListaItemDetails(
+    id = id,
+    name = descrip,
+
+)
+
