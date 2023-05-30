7dbf088c7308f38dbde526551d477393e57b4c40
Carlos Melendez <carlosmelendezr@gmail.com>
2023-05-30 06:32:36 -0400

    Cambios Edit 2

--- app/src/main/java/com/veramed/inventario/ui/lista/ListaDetalleScreen.kt
+++ app/src/main/java/com/veramed/inventario/ui/lista/ListaDetalleScreen.kt
@@ -56,13 +56,12 @@ fun ListaDetalleScreen(
         ItemDetallesBody(
             itemDetailsUiState = uiState.value,
             datosVence = viewModel.venceUiState ,
-            onSaveItem =  viewModel::saveItem ,
+            onSaveItem =  { viewModel::saveItem
+                            navigateBack()
+                          },
             onValueChange = viewModel::actualizaUiState,
             onDelete = {
-                // Note: If the user rotates the screen very fast, the operation may get cancelled
-                // and the item may not be deleted from the Database. This is because when config
-                // change occurs, the Activity will be recreated and the rememberCoroutineScope will
-                // be cancelled - since the scope is bound to composition.
+
                 coroutineScope.launch {
                     viewModel.deleteItem()
                     navigateBack()
